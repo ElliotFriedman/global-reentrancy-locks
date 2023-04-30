6920a414ae3d098114b55a352d8780e87605a585
Elliot <elliotfriedman3@gmail.com>
2023-04-29 22:19:50 -0700

    example global reentrancy lock

---
+++ src/example/ExampleGlobalReentrancyLock.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.13;
+
+import {BaseGlobalReentrancyLock} from "./../BaseGlobalReentrancyLock.sol";
+
+contract ExampleGlobalReentrancyLock is GlobalReentrancyLock {
+
+}
